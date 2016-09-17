eb40bdd7be256f51e333d282e85dac379fb48502
rdartus <richard.dartus@epfedu.fr>
2016-09-17 02:54:19 +0200

    test6

--- devServer.js
+++ devServer.js
@@ -8,6 +8,8 @@ var compiler = webpack(config);
 
 
 var theport = process.env.PORT || 8000;
+var server_host = process.env.YOUR_HOST || 'localhost';
+
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
   publicPath: config.output.publicPath
@@ -19,14 +21,14 @@ app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(theport, 'localhost', function(err) {
+app.listen(theport, server_host, function(err) {
 
   console.log(__dirname);
-  
+
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:'+theport);
+  console.log('Listening at http://'server_host+theport);
 });
